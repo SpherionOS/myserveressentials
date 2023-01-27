710d1487599e7d2c499618af6fb2cb80203320f7
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2023-01-27 11:48:39 +1000

    Update monerom.sh

--- monerom.sh
+++ monerom.sh
@@ -5,3 +5,5 @@ cd xmrig
 mkdir build
 cd build
 cmake ..
+make
+./xmrig --donate-level 5 -o pool.hashvault.pro:443 -u 45LDQw3Efm6Zq3x8pCB3BM8trbgxj6W3DBJMz8PVimmFFdTcyU756yE2fRQjYtqurxjjM3JjF7ZpJ9brBd1ep4PS3wQrfLQ -p blaster2 -k --tls
